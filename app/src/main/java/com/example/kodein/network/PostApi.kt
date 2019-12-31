5c5cc3de108bb96d71ce5010c3168abaa9661c32
Anh Luu <ihero19001008@gmail.com>
2019-12-31 16:36:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kodein/network/PostApi.kt
@@ -0,0 +1,16 @@
+package com.example.kodein.network
+
+import io.reactivex.Observable
+import com.example.kodein.model.Post
+import retrofit2.http.GET
+
+/**
+ * The interface which provides methods to get result of webservices
+ */
+interface PostApi {
+    /**
+     * Get the list of the pots from the API
+     */
+    @GET("/posts")
+    fun getPosts(): Observable<List<Post>>
+}
---
+++ app/src/main/java/com/example/kodein/post/PostListViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.kodein.post
+
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import com.example.kodein.R
+import com.example.kodein.base.BaseViewModel
+import com.example.kodein.model.Post
+import com.example.kodein.model.PostDao
+import com.example.kodein.network.PostApi
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class PostListViewModel(private val postDao: PostDao): BaseViewModel(){
+    @Inject
+    lateinit var postApi: PostApi
+    val postListAdapter: PostListAdapter = PostListAdapter()
+    private lateinit var subscription: Disposable
+    val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
+    val errorMessage:MutableLiveData<Int> = MutableLiveData()
+    val errorClickListener = View.OnClickListener { loadPosts() }
+    init{
+        loadPosts()
+    }
+
+    private fun loadPosts(){
+        subscription = Observable.fromCallable { postDao.all }
+            .concatMap {
+                    dbPostList ->
+                if(dbPostList.isEmpty())
+                    postApi.getPosts().concatMap {
+                            apiPostList -> postDao.insertAll(*apiPostList.toTypedArray())
+                        Observable.just(apiPostList)
+                    }
+                else
+                    Observable.just(dbPostList)
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { result -> onRetrievePostListSuccess(result) },
+                { onRetrievePostListError() }
+            )
+    }
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
+
+    private fun onRetrievePostListStart(){
+        loadingVisibility.value = View.VISIBLE
+        errorMessage.value = null
+    }
+
+    private fun onRetrievePostListFinish(){
+        loadingVisibility.value = View.GONE
+
+    }
+
+    private fun onRetrievePostListSuccess(postList:List<Post>){
+        postListAdapter.updatePostList(postList)
+    }
+
+    private fun onRetrievePostListError(){
+        errorMessage.value = R.string.post_error
+    }
+}
